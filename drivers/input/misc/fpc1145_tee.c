27e88bd1bb608f6a44c603f960f5b1a714b90f32
franciscofranco <franciscofranco.1990@gmail.com>
2016-10-09 16:59:49 +0200

    input: fpc1145_tee: boost tasks to the big cores when the scanner is used
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/input/misc/fpc1145_tee.c
+++ drivers/input/misc/fpc1145_tee.c
@@ -605,10 +605,12 @@ static irqreturn_t fpc1145_irq_handler(int irq, void *handle)
 	dev_dbg(fpc1145->dev, "%s %d\n", __func__, fpc1145->irq_num);
 
 	if (!is_display_on()) {
+		sched_set_boost(1);
 		input_report_key(fpc1145->input_dev, KEY_FINGERPRINT, 1);
 		input_sync(fpc1145->input_dev);
 		input_report_key(fpc1145->input_dev, KEY_FINGERPRINT, 0);
 		input_sync(fpc1145->input_dev);
+		sched_set_boost(0);
 	}
 
 	return IRQ_HANDLED;
