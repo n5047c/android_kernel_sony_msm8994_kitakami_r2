fda4ad2d1ba1eb395da2ebb62f2b8505974d5455
franciscofranco <franciscofranco.1990@gmail.com>
2016-10-09 16:57:50 +0200

    input: fpc1145_tee: boost cpufreq on unlock
    To reduce wake-up latency when using the fingerprint scanner we
    register an input device and send a "key press" when the scanner
    is being used during screen off. This will boost the cpus through
    cpu-boost and reduce the screen on latency.
    
    General idea "stole" from this commit sultanxda/android_kernel_oneplus_msm8996@f194585
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    
    this needs the commit
    
    'display: add a simple api to query the display state (on/off) at any point in time'
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/input/misc/fpc1145_tee.c
+++ drivers/input/misc/fpc1145_tee.c
@@ -50,6 +50,11 @@
 #include <linux/regulator/consumer.h>
 #include <linux/spi/spi.h>
 #include <soc/qcom/scm.h>
+#include <linux/input.h>
+#include <linux/display_state.h>
+
+/* Unused key value to avoid interfering with active keys */
+#define KEY_FINGERPRINT 0x2ee
 
 #define FPC1145_RESET_LOW_US 1000
 #define FPC1145_RESET_HIGH1_US 100
@@ -99,6 +104,7 @@ struct fpc1145_data {
 	int event_code;
 	struct mutex lock;
 	bool prepared;
+	struct input_dev *input_dev;
 };
 
 static int vreg_setup(struct fpc1145_data *fpc1145, const char *name,
@@ -561,12 +567,50 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
+static int fpc1145_input_init(struct fpc1145_data * fpc1145)
+{
+	int ret;
+
+	fpc1145->input_dev = input_allocate_device();
+	if (!fpc1145->input_dev) {
+		pr_err("fingerprint input boost allocation is fucked - 1 star\n");
+		ret = -ENOMEM;
+		goto exit;
+	}
+
+	fpc1145->input_dev->name = "fpc1145";
+	fpc1145->input_dev->evbit[0] = BIT(EV_KEY);
+
+	set_bit(KEY_FINGERPRINT, fpc1145->input_dev->keybit);
+
+	ret = input_register_device(fpc1145->input_dev);
+	if (ret) {
+		pr_err("fingerprint boost input registration is fucked - fixpls\n");
+		goto err_free_dev;
+	}
+
+	return 0;
+
+err_free_dev:
+	input_free_device(fpc1145->input_dev);
+exit:
+	return ret;
+}
+
 static irqreturn_t fpc1145_irq_handler(int irq, void *handle)
 {
 	struct fpc1145_data *fpc1145 = handle;
 	input_event(fpc1145->idev, EV_MSC, MSC_SCAN, ++fpc1145->irq_num);
 	input_sync(fpc1145->idev);
 	dev_dbg(fpc1145->dev, "%s %d\n", __func__, fpc1145->irq_num);
+
+	if (!is_display_on()) {
+		input_report_key(fpc1145->input_dev, KEY_FINGERPRINT, 1);
+		input_sync(fpc1145->input_dev);
+		input_report_key(fpc1145->input_dev, KEY_FINGERPRINT, 0);
+		input_sync(fpc1145->input_dev);
+	}
+
 	return IRQ_HANDLED;
 }
 
@@ -729,6 +773,10 @@ static int fpc1145_probe(struct spi_device *spi)
 		goto exit;
 	}
 
+	rc = fpc1145_input_init(fpc1145);
+	if (rc)
+		goto exit;
+
 	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
 	if (of_property_read_bool(dev->of_node, "fpc,enable-wakeup")) {
 		irqf |= IRQF_NO_SUSPEND;
@@ -766,6 +814,9 @@ exit:
 static int fpc1145_remove(struct spi_device *spi)
 {
 	struct  fpc1145_data *fpc1145 = dev_get_drvdata(&spi->dev);
+
+	if (fpc1145->input_dev != NULL)
+		input_free_device(fpc1145->input_dev);
 
 	sysfs_remove_group(&spi->dev.kobj, &attribute_group);
 	mutex_destroy(&fpc1145->lock);
